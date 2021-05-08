a8a08ac1a7ca6b4b634c81c845814ff86c54a6d8
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-05-08 14:40:26 +0300

    Добавлен функционал рисования

--- js/drawer.js
+++ js/drawer.js
@@ -4,13 +4,45 @@ canvas.width  = 650;
 canvas.height = 750;
 var list = document.getElementById("Choise");
 var image = new Image();
-image.src = "src/start.png"
-image.onload = function() {
-	ctx.drawImage(image, 50, 50);
-}
+
+var x = 275;
+var y = 50;
 
 
 //Выбор значения из списка
 list.onchange = function() {
 	alert(list.value);
+}
+
+var button_down = document.getElementById('but2');
+button_down.onclick = function() {
+	ctx.clearRect(x, y, 100, 51);
+	y += 10;
+	ctx.drawImage(image, x, y);
+}
+
+var button_up = document.getElementById('but1');
+button_up.onclick = function() {
+	ctx.clearRect(x, y, 100, 51);
+	y -= 10;
+	ctx.drawImage(image, x, y);
+}
+
+var button_left = document.getElementById('but3');
+button_left.onclick = function() {
+	ctx.clearRect(x, y, 100, 51);
+	x -= 10;
+	ctx.drawImage(image, x, y);
+}
+
+var button_right = document.getElementById('but4');
+button_right.onclick = function() {
+	ctx.clearRect(x, y, 100, 51);
+	x += 10;
+	ctx.drawImage(image, x, y);
+}
+
+var button_enter = document.getElementById('but5');
+button_enter.onclick = function() {
+	image.src = list.value;
 }
